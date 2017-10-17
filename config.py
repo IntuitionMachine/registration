c9f07bb7caa2083919af99e54f8dcca1ee33df21
eduardofvaldez1@gmail.com <eduardofvaldez1@gmail.com>
2017-10-17 10:03:59 +0000

    edited config file

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 #SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
 #LOCAL DB
 #SQLALCHEMY_DATABASE_URI = ''
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 #SQLALCHEMY_DATABASE_URI='postgresql://postgres:admin@localhost/intuitionmachine'
 #Recaptcha Settings
 #RECAPTCHA_USE_SSL = False
