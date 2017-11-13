4c460e7033102f0af8d817c2a246ea0b5d68d661
eduardofvaldez1@gmail.com <eduardofvaldez1@gmail.com>
2017-11-13 07:40:59 +0000

    Changed directory for training data

--- app/train.py
+++ app/train.py
@@ -8,14 +8,18 @@ from rasa_nlu.model import Metadata, Interpreter
 
 
 builder = ComponentBuilder(use_cache=True)
-training_data = load_data('/var/www/new/registration/data/examples/rasa/demo-rasa.json')
+#training_data = load_data('/var/www/new/registration/data/examples/rasa/demo-rasa.json')
+training_data = load_data('/registration/data/examples/rasa/demo-rasa.json')
+#trainer = Trainer(RasaNLUConfig("/var/www/new/registration/sample_configs/config_spacy.json"),builder)
+trainer = Trainer(RasaNLUConfig("/registraiton/sample_configs/config_spacy.json"),builder)
 
-trainer = Trainer(RasaNLUConfig("/var/www/new/registration/sample_configs/config_spacy.json"),builder)
 trainer.train(training_data)
-model_directory = trainer.persist('/var/www/new/registration/projects/default/')
+#model_directory = trainer.persist('/var/www/new/registration/projects/default/')
+model_directory = trainer.persist('/registration/projects/default/')
 print ('dir')
 print(model_directory)
-config= RasaNLUConfig("/var/www/new/registration/sample_configs/config_spacy.json")
+#config= RasaNLUConfig("/var/www/new/registration/sample_configs/config_spacy.json")
+config = RasaNLUConfig("/registration/sample_configs/config_spacy.json")
 interpreter = Interpreter.load(model_directory,config,builder)
 
 print (interpreter.parse(u'carlos'))
